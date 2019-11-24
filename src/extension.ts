2e60d3d01ff9b3318c3b23665dff56f1208a432f
Qi Zhang <singularitti@outlook.com>
2019-11-24 18:03:23 -0500

    Copy sample code from example
    https://github.com/jrieken/vscode-formatter-sample/blob/master/src/extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,16 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+'use strict';
+
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-julia-formatter" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	vscode.languages.registerDocumentFormattingEditProvider('foo-lang', {
+		provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+			const firstLine = document.lineAt(0);
+			if (firstLine.text !== '42') {
+				return [vscode.TextEdit.insert(firstLine.range.start, '42\n')];
+			}
+		}
 	});
-
-	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
